a48db0a13d3d7735804e3f93e35e11539cb1d410
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-08-09 21:31:35 -0400

    bootstrapped app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// first we will make a new context
+const MyContext = React.createContext('default');
+
+// Then create a provider Component
+class MyProvider extends Component {
+  state = {
+    name: 'Nate',
+    age: 100,
+    cool: true
+  };
+
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          increment: () =>
+            this.setState(prevState => {
+              return { age: prevState.age + 1 };
+            }),
+          changeName: inputFromChild => {
+            this.setState({
+              name: inputFromChild
+            });
+          }
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+const Family = props => (
+  <div className="family">
+    <Person />
+  </div>
+);
+
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: '' };
+  }
+  render() {
+    let { input } = this.state;
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {context => (
+            <React.Fragment>
+              <p>Age: {context.state.age}</p>
+              <button onClick={context.increment}>+</button>
+              <p>Name: {context.state.name}</p>
+              <form
+                onSubmit={e => {
+                  e.preventDefault();
+                  context.changeName(input);
+                }}
+              >
+                <input
+                  type="text"
+                  value={this.state.value}
+                  onChange={e => context.changeName(e.target.value)}
+                />
+                <button type="submit">Change Name</button>
+              </form>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MyProvider>
+        <div>
+          <p>I am the app</p>
+          <Family />
+        </div>
+      </MyProvider>
     );
   }
 }
